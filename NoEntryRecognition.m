b8789d1d346db69a99e7d17823a9d09ab1b8c03b
jfrancis71 <julian.w.francis@gmail.com>
2015-01-04 11:27:11 +0000

    Complete Redesign
    Pixel based appearance model with a contrast detector
    Achieves 5 false positives and 13/14 true positives, tested on 1-9
    positives

--- NoEntryRecognition.m
+++ NoEntryRecognition.m
@@ -15,73 +15,54 @@ negativeFiles=FileNames["C:\\Users\\Julian\\secure\\Shape Recognition\\Stop Sign
 negatives=Map[StandardiseImage[#,640]&,negativeFiles];
 
 
-barAngle=Table[
-   If[Abs[x]<6,If[y==1,-\[Pi]/2,If[y==-1,\[Pi]/2,0]],0]
-,{y,-8,8},{x,-8,8}];
+NoEntrySignMask=Table[
+   If[Sqrt[x^2+y^2]<=6.0,1.0,0.0]
+      ,{y,-8,+8},{x,-8,+8}];
+
+NoEntryEdgeKernel=Table[If[(7<Sqrt[y^2+x^2]<=8),1.0,0.0],{y,-8,+8},{x,-8,+8}];
+NoEntrySignExt=Position[NoEntryEdgeKernel,1.];
+NoEntrySignInt=Map[Function[c,SortBy[Position[NoEntrySignMask,1.],EuclideanDistance[#,c]&]//First],Position[NoEntryEdgeKernel,1.]];
+
+MVShapeNoEntryFilt[patch_]:=Total[Clip[(Extract[patch,NoEntrySignExt]-Extract[patch,NoEntrySignInt])^2,{0,0.2^2}]]
 
 
-barKernel=Table[
-   If[Abs[x]<6,If[y==1||y==-1,1,0],0]
-,{y,-8,+8},{x,-8,+8}];
+NoEntrySignKernel=Table[
+   If[Sqrt[x^2+y^2]<=6.0,If[Abs[x]<=5&&Abs[y]<=1,.8,.8*.4],0.0]
+      ,{y,-8,+8},{x,-8,+8}];
 
 
-BrightBarSurfMagDirPatchH[patch_]:=
-   Log[2,HNDist[patch[[All,All,1]],1.6](*p[Edge Mag|E=1*)*
-   BesI0s5*E^(05.0 Cos[patch[[All,All,2]]-barAngle]) * (*p[Edge dir|E=1*)
-   barKernel (*p[E=1]*)] -
-   Log[2,HNDist[patch[[All,All,1]],11.1]*1/(2 \[Pi])]*barKernel;
-BrightBarSurfMagDirPatch[patch_]:=BrightBarSurfMagDirPatch[patch]
-
-
-mylog2[x_]=N[1.0/(1.0+Exp[-(-800.0+ x)]),5];
-
-
-SetAttributes[mylog2,Listable];
-SetAttributes[mylog2,NumericFunction];
-
-
-BrightBarPatchH[patch_]:=Cos[(patch-barAngle)]*barKernel;
-BrightBarPatch[patch_]:=Total[BrightBarPatchH[patch],2]//mylog2
-
-
-NoEntryRecognition[image_?MatrixQ]:= (
-   imgPyramid=BuildPyramid[image,{8,8}];
-Print["Build Pyr"];
-   edgeMagDir=SobelFilter[imgPyramid,EdgeMagDir];
-   surfPyr=EdgeMagDirToSurfDir[edgeMagDir];
-Print["Build Surf Pyr"];
-   edgeDirPyr=EdgeMagDirToEdgeDir[edgeMagDir];
-Print["Build Edge Dir Pyr"];
-   edgeMagPyr=EdgeMagDirToEdgeMag[edgeMagDir];
-Print["Build Edge Mag Pyr"];
-   circleFilterOutput=SurfCircleConv[surfPyr];
-Print["Build Circle Pyr"];
-(*
-   barOutput=
-      MVCorrelatePyramid[Cos[2.0*surfPyr], barKernel*Cos[2.0*barAngle]]+
-      MVCorrelatePyramid[Sin[2.0*surfPyr], barKernel*Sin[2.0*barAngle]];
-  
-*)
-   barOutput=EdgeDirConv[edgeDirPyr,barKernel,barAngle]
-     +MVCorrelatePyramid[Log[2,HNDist[edgeMagPyr,1.6]],barKernel]
-     -MVCorrelatePyramid[Log[2,0.57*HNDist[edgeMagPyr,11.2]+(1.0-0.57)*HNDist[edgeMagPyr,1.6]],barKernel];
-Print["Build bar"];
-(*   circleFilterOutput*mylog2[barOutput];*)
-
-   pl1=circleFilterOutput;
-Print["Build pl1"];
-   pr1=1.0/(1.0+Exp[-(-40.0+barOutput)]); (*Unfortunate can't get mylog to work machine precision *)
-Print["Build mylog2"];
-   pl2 = 0.02*pl1 + 10^-7*(1-pl1);
-   pr2 = 0.0005*pr1 + 10^-7*(1-pr1);
-
-   pf=10^-6;
-  res=(pf*(1-pf))/(pl2*pr2*(1-pf) + (1-pl2)*(1-pr2)*pf)*(pl2*pr2)/pf;
-Print["Completed"];
-res
+AppearanceNoEntryConvFast[pyr_]:=(
+   S1=MVCorrelatePyramid[pyr^2,
+   NoEntrySignMask];
+   S2=MVCorrelatePyramid[pyr,NoEntrySignKernel];
+   S3=Total[NoEntrySignKernel^2,2];
+   (-1+Log[Exp[-S1] Exp[S2^2/S3]* ( Erf[(S3-S2)/Sqrt[S3]] + Erf[(S2-0.25 S3)/Sqrt[S3]] )])
 )
 
 
-NoEntryRecognitionOutput[image_?MatrixQ,threshold_:.2]:= (
+erasePyr=Map[ConstantArray[1.,Dimensions[#]]&,BuildPyramid[positives[[1]],{8,8}][[1;;-1]]];
+Table[erasePyr[[l,1;;8,All]]=0.,{l,1,Length[erasePyr]}];
+Table[erasePyr[[l,-8;;-1,All]]=0.,{l,1,Length[erasePyr]}];
+Table[erasePyr[[l,All,1;;8]]=0.,{l,1,Length[erasePyr]}];
+Table[erasePyr[[l,All,-8;;-1]]=0.,{l,1,Length[erasePyr]}];
+
+
+NoEntryRecognition[image_?MatrixQ]:=(
+   pyr=BuildPyramid[image,{8,8}];
+
+   rawShape=PyramidFilter[MVShapeNoEntryFilt, pyr, {8,8}];
+   shape=1/(1+Exp[-(rawShape-.58)*50]);
+   shape = shape*erasePyr;
+
+   rawApp=AppearanceNoEntryConvFast[pyr];
+ (* 120 *)
+   appearance=1/(1+Exp[-(rawApp+0.9)*50]);
+   appearance=appearance*erasePyr;
+
+   res=appearance*shape
+)
+
+
+NoEntryRecognitionOutput[image_?MatrixQ,threshold_:.5]:= (
    output=NoEntryRecognition[image];
    Show[image//DispImage,BoundingRectangles[output,threshold,{8,8}]//OutlineGraphics] )
