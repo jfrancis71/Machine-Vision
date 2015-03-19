4de6ad3a5dab3a514bdb44b88beeca0caaa482d8
jfrancis71 <julian.w.francis@gmail.com>
2015-03-19 17:39:48 +0000

    initial revision

---
+++ FeatureDetectors.m
@@ -0,0 +1,13 @@
+(* ::Package:: *)
+
+(*
+Computes log p(Image|H=1)
+Implicity assumes sigma=0.05 with an independant
+gaussian model per pixel
+It is scaled by \[Lambda]
+Note this means \[Lambda]=0 will strongly match a blank patch
+*)
+TemplateDetector[pyr_?PyramidImageQ,kernel_?MatrixQ,mask_?MatrixQ,\[Lambda]_?PyramidImageQ]:=
+   Log[1/(Sqrt[2 \[Pi]] .05)] - 1/(2 .05^2) (MVCorrelatePyramid[pyr^2,mask] +
+      -2 \[Lambda] MVCorrelatePyramid[pyr,kernel*mask] +
+      \[Lambda]^2 Total[(kernel*mask)^2,2])
