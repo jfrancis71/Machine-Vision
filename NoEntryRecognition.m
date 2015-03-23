cffb1b0b8985fc96e4e38db2dce812f3dff71b22
jfrancis71 <julian.w.francis@gmail.com>
2015-03-23 08:46:57 +0000

    Major revision
    Use two lighting models, one lambda for the central bar, and a related
    gamma for the background of the sign (to cope with specular reflection).

--- NoEntryRecognition.m
+++ NoEntryRecognition.m
@@ -1,6 +1,7 @@
 (* ::Package:: *)
 
 <<"C:/users/julian/documents/github/Machine-Vision/MVTools.m"
+<<"C:/users/julian/documents/github/Machine-Vision/FeatureDetectors.m"
 
 
 positiveFiles=FileNames["C:\\Users\\Julian\\secure\\Shape Recognition\\Stop Sign\\Training Data\\Positives\\*"];
@@ -15,130 +16,90 @@ negativeFiles=FileNames["C:\\Users\\Julian\\secure\\Shape Recognition\\Stop Sign
 negatives=Map[StandardiseImage[#,640]&,negativeFiles];
 
 
-NoEntrySignMask=Table[
-   If[Sqrt[x^2+y^2]<=6.0,1.0,0.0]
-      ,{y,-8,+8},{x,-8,+8}];
-
-NoEntryEdgeKernel=Table[If[(7<Sqrt[y^2+x^2]<=8),1.0,0.0],{y,-8,+8},{x,-8,+8}];
-NoEntrySignExt=Position[NoEntryEdgeKernel,1.];
-NoEntrySignInt=Map[Function[c,SortBy[Position[NoEntrySignMask,1.],EuclideanDistance[#,c]&]//First],Position[NoEntryEdgeKernel,1.]];
-
-MVShapeNoEntryFilt[patch_]:=Total[Clip[(Extract[patch,NoEntrySignExt]-Extract[patch,NoEntrySignInt])^2,{0,0.2^2}]]
+ImagesQ[images_]:=MatrixQ[images[[1]]];
 
 
-AngleDiff[a_,b_]:=Min[Abs[a-b],2 \[Pi] - Abs[a-b]]
+CentreMask=ArrayPad[ConstantArray[1,{3,13}],{{2,2},{0,0}}];
 
 
-NoEntryBoundaryKernel=Table[If[(6<Sqrt[y^2+x^2]<=7),1.0,0.0],{y,-8,+8},{x,-8,+8}];
+CentreKernel=CentreMask;
 
 
-NoEntryEdgeVKernel=Table[If[x==0&&y==0,0,If[
-   AngleDiff[\[Pi]/2,ArcTan[x,y]]<((\[Pi]/2)/3)||AngleDiff[-\[Pi]/2,ArcTan[x,y]]<((\[Pi]/2)/3)
-      ,1.,0.]],{y,-8,+8},{x,-8,+8}]*NoEntryBoundaryKernel;
+ExtBarMask=ArrayPad[ConstantArray[0,{3,13}],{{2,2},{1,1}},1];
 
 
-NoEntryEdgeHKernel=Table[If[x==0&&y==0,0,If[
-   AngleDiff[\[Pi],ArcTan[x,y]]<((\[Pi]/2)/3)||AngleDiff[0,ArcTan[x,y]]<((\[Pi]/2)/3)
-      ,1.,0.]],{y,-8,+8},{x,-8,+8}]*NoEntryBoundaryKernel;
+ExtBarKernel=ExtBarMask;
 
 
-NoEntryEdgeDiag1Kernel=Table[If[x==0&&y==0,0,If[
-   AngleDiff[\[Pi]/4,ArcTan[x,y]]<((\[Pi]/2)/3)-.2||AngleDiff[-3 \[Pi]/4,ArcTan[x,y]]<((\[Pi]/2)/3)-.2
-      ,1.,0.]],{y,-8,+8},{x,-8,+8}]*NoEntryBoundaryKernel;
+ExtBarMask=ArrayPad[ConstantArray[0,{5,13}],{{1,1},{0,0}},1];
 
 
-NoEntryEdgeDiag2Kernel=Table[If[x==0&&y==0,0,If[
-   AngleDiff[3 \[Pi]/4,ArcTan[x,y]]<((\[Pi]/2)/3)-.2||AngleDiff[- \[Pi]/4,ArcTan[x,y]]<((\[Pi]/2)/3)-.2
-      ,1.,0.]],{y,-8,+8},{x,-8,+8}]*NoEntryBoundaryKernel;
+ExtBarKernel=ExtBarMask;
 
 
-ShapeNoEntryPatch[patch_]:=-(
-Log[
-   0.3*(15.9/(1.+(2500.*patch[[1]] ))) + 0.7
-      ]*NoEntryEdgeHKernel+
-Log[
-   0.3*(15.9/(1.+(2500.*patch[[2]] ))) + 0.7
-      ]*NoEntryEdgeVKernel+
-Log[
-   0.3*(15.9/(1.+(2500.*patch[[3]] ))) + 0.7
-      ]*NoEntryEdgeDiag1Kernel+
-Log[
-   0.3*(15.9/(1.+(2500.*patch[[4]] ))) + 0.7
-      ]*NoEntryEdgeDiag2Kernel
-)
+altMask=Clip[CentreMask+ExtBarMask,{0,1}];
 
 
-(* ::Text:: *)
-(*PDF[StudentTDistribution[0,.02,1],d]  =  15.9155/(1+2500. d^2)*)
+altKernel=altMask;
 
 
-ShapeNoEntryConv[pyr_]:=(
-   horizDiff=MVCorrelatePyramid[pyr,{{1,0,-1}}]^2;
-   vertDiff=MVCorrelatePyramid[pyr,Transpose[{{1,0,-1}}]]^2;
-   diag1Diff=MVCorrelatePyramid[pyr,{{0,0,1},{0,0,0},{-1,0,0}}//Reverse]^2;
-   diag2Diff=MVCorrelatePyramid[pyr,{{0,0,1},{0,0,0},{-1,0,0}}]^2;
-   -(
-    MVCorrelatePyramid[Log[0.3*15.9/(1+horizDiff*2500)+0.7],NoEntryEdgeHKernel]+
-    MVCorrelatePyramid[Log[0.3*15.9/(1+vertDiff*2500)+0.7],NoEntryEdgeVKernel]+
-    MVCorrelatePyramid[Log[0.3*15.9/(1+diag1Diff*2500)+0.7],NoEntryEdgeDiag1Kernel]+
-    MVCorrelatePyramid[Log[0.3*15.9/(1+diag2Diff*2500)+0.7],NoEntryEdgeDiag2Kernel]
-)
-)
+maskK={ConstantArray[0,21]};maskK[[1,1]]=1;maskK[[1,7]]=1;edgeK=maskK;edgeK[[1,7]]=-1;
 
 
-NoEntrySignKernel=Table[
-   If[Sqrt[x^2+y^2]<=6.0,If[Abs[x]<=5&&Abs[y]<=1,.8,.8*.4],0.0]
-      ,{y,-8,+8},{x,-8,+8}];
+myMax[a_,b_]:=Max[a,b]
 
 
-AppearanceNoEntryConvFast[pyr_]:=(
-   S1=MVCorrelatePyramid[pyr^2,
-   NoEntrySignMask];
-   S2=MVCorrelatePyramid[pyr,NoEntrySignKernel];
-   S3=Total[NoEntrySignKernel^2,2];
-   (-1+Log[Exp[-S1] Exp[S2^2/S3]* ( Erf[(S3-S2)/Sqrt[S3]] + Erf[(S2-0.25 S3)/Sqrt[S3]] )]);
-
-   S1 = (1./(2. .0025)) * MVCorrelatePyramid[pyr^2,NoEntrySignMask];
-   S2 = (1./(2. .0025)) * MVCorrelatePyramid[pyr,NoEntrySignKernel];
-   S3 = (1./(2. .0025)) * Total[NoEntrySignKernel^2,2];
-
-   P1 = ( Erf[(S3-S2)/Sqrt[S3]] + Erf[(S2 - 0.25 S3)/Sqrt[S3]] );
-   P2 = Exp[-S1] Exp[S2^2/S3];
-   (*Log[1./(Sqrt[2 3.14] .05)^81 .886 * Exp[-S1] Exp[S2^2/S3] ( Erf[(S3-S2)/Sqrt[S3]] + Erf[S2/Sqrt[S3]] )]- 81 Log[3]*)
-   (*Log[1./(Sqrt[2 3.14] .05)^81] - 81 Log[3] + Log[.886] + -S1 + S2^2/S3 + 0.7*)
-   Log[(1./(Sqrt[2 3.14] .05)^113) 1.77245 * P2 P1 ]- Log[2 Sqrt[S3]] - 113 Log[3]
-)
+SetAttributes[myMax,Listable]
 
 
-AppearanceNoEntryPatchHelp[patch_]:=(
-Log[Sum[1/((1-.25)/.01) Exp[(-1/(2. .0025)) Sum[NoEntrySignMask[[y,x]] (patch[[y,x]] - \[Alpha] NoEntrySignKernel[[y,x]])^2,{y,1,17},{x,1,17}]],{\[Alpha],.25,1,.01}]]
-)
+NoEntryRecognition[images_?ImagesQ]:=(
+   pyrs=Map[BuildPyramid,positives[[1;;9]]];
+   \[Lambda]=Map[MVCorrelatePyramid[#,{{1,1,1}}]/3.&,pyrs];
+   \[Nu]=Map[MVCorrelatePyramid[#,{{0,0,0},{0,0,0},{0,0,0},{1,1,1}}]/3.&,pyrs];
+   centre=Table[TemplateDetector[pyrs[[w]],CentreKernel,CentreMask,\[Lambda][[w]]],{w,1,Length[images]}];
+   ext1=Table[TemplateDetector[pyrs[[w]],ExtBarKernel,ExtBarMask,\[Lambda][[w]]*.33],{w,1,Length[images]}];
+   ext2=Table[TemplateDetector[pyrs[[w]],ExtBarKernel,ExtBarMask,\[Lambda][[w]]*.52],{w,1,Length[images]}];
+   ext3=Table[TemplateDetector[pyrs[[w]],ExtBarKernel,ExtBarMask,\[Lambda][[w]]*.41],{w,1,Length[images]}];
+   extBar=myMax[ext1,myMax[ext2,ext3]];
+   alt=myMax[
+   ct1=Table[TemplateDetector[pyrs[[w]],altKernel,altMask,\[Lambda][[w]]],{w,1,Length[images]}],
+   ct2=Table[TemplateDetector[pyrs[[w]],altKernel*.1,altMask],{w,1,Length[images]}]
+      ];
+   edgeBarL=Table[MVCorrelatePyramid[pyrs[[w]],edgeK],{w,1,Length[images]}];lEdgeBar=2*Log[NeighbourProb[0,edgeBarL]];
+   edgeBarR=Table[MVCorrelatePyramid[pyrs[[w]],Map[Reverse,edgeK]],{w,1,Length[images]}];rEdgeBar=2*Log[NeighbourProb[0,edgeBarR]];
+   app=centre+extBar;
+   alt2=myMax[alt,app+Log[.00001]];
+   tot=(Log[.001]+app)-Map[Max[#,115]&,(alt2 + Log[.999]),{4}]-lEdgeBar-rEdgeBar;
+   tot=tot*UnitStep[\[Lambda]-.25])
 
-AppearanceNoEntryPatch[patch_]:=Log[10^-20 + 1./(Sqrt[2 3.14] .05)^113 .886 ] + AppearanceNoEntryPatchHelp[patch] - 113 Log[3]
 
 
-erasePyr=Map[ConstantArray[1.,Dimensions[#]]&,BuildPyramid[positives[[1]],{8,8}][[1;;-1]]];
-Table[erasePyr[[l,1;;8,All]]=0.,{l,1,Length[erasePyr]}];
-Table[erasePyr[[l,-8;;-1,All]]=0.,{l,1,Length[erasePyr]}];
-Table[erasePyr[[l,All,1;;8]]=0.,{l,1,Length[erasePyr]}];
-Table[erasePyr[[l,All,-8;;-1]]=0.,{l,1,Length[erasePyr]}];
+   disp[p_]:=Show[positives[[p]]//DispImage,BoundingRectangles[tot[[p]],4.,{10,10}]//OutlineGraphics] 
+
+
+NoEntryRecognitionOutput[images_?ImagesQ]:=(
+   NoEntryRecognition[images];
+   Table[disp[p],{p,1,9}])
 
 
 NoEntryRecognition[image_?MatrixQ]:=(
-   pyr=BuildPyramid[image,{8,8}];
-
-   shape=ShapeNoEntryConv[pyr];
-
-   appearance=AppearanceNoEntryConvFast[pyr];
- (* 120 *)
-   
-   res=appearance+shape
-)
+   NoEntryRecognition[{image}]
+);
 
 
-NoEntryRecognitionOutput[image_?MatrixQ,threshold_:.5]:= (
-   output=NoEntryRecognition[image];
-   Show[image//DispImage,BoundingRectangles[output,threshold,{8,8}]//OutlineGraphics] )
+NoEntryRecognitionOutput[image_?MatrixQ,threshold_:4.]:= (
+   output=NoEntryRecognition[{image}];
+   Show[image//DispImage,BoundingRectangles[output,threshold,{8,8}]//OutlineGraphics] );
+
+
+DisplayPatches[]:=(
+   conc=Select[Position[tot,x_/;x>4.0],InBoundsQ[pyrs[[1]],#[[2]],#[[3]],#[[4]],10]&];
+   SortBy[Map[
+   {#,
+   tot[[#[[1]],#[[2]],#[[3]],#[[4]]]],
+   app[[#[[1]],#[[2]],#[[3]],#[[4]]]],
+   alt2[[#[[1]],#[[2]],#[[3]],#[[4]]]],
+   (Patch[pyrs[[#[[1]]]],#[[2]],#[[3]],#[[4]],10]//DispImage)}&,conc],#[[2]]&]);
 
 
 GroundTruth=Table[{},{10}];
