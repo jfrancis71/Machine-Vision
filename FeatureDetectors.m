a41a70a4606ff579615bf4fe6518d27dcec68947
jfrancis71 <julian.w.francis@gmail.com>
2015-03-23 08:12:17 +0000

    Added unscaled lambda template detector
    Is basically a box standard template detector

--- FeatureDetectors.m
+++ FeatureDetectors.m
@@ -7,7 +7,12 @@ gaussian model per pixel
 It is scaled by \[Lambda]
 Note this means \[Lambda]=0 will strongly match a blank patch
 *)
+TemplateDetector[pyr_?PyramidImageQ,kernel_?MatrixQ,mask_?MatrixQ]:=
+   Total[mask,2]*Log[1/(Sqrt[2 \[Pi]] .05)] - 1/(2 .05^2) (MVCorrelatePyramid[pyr^2,mask] +
+      -2 MVCorrelatePyramid[pyr,kernel*mask] +
+      Total[(kernel*mask)^2,2])
+
 TemplateDetector[pyr_?PyramidImageQ,kernel_?MatrixQ,mask_?MatrixQ,\[Lambda]_?PyramidImageQ]:=
-   Log[1/(Sqrt[2 \[Pi]] .05)] - 1/(2 .05^2) (MVCorrelatePyramid[pyr^2,mask] +
+   Total[mask,2]*Log[1/(Sqrt[2 \[Pi]] .05)] - 1/(2 .05^2) (MVCorrelatePyramid[pyr^2,mask] +
       -2 \[Lambda] MVCorrelatePyramid[pyr,kernel*mask] +
       \[Lambda]^2 Total[(kernel*mask)^2,2])
