dcaf6b8f1a2e4426d163ffb01bb8925805d3d506
jfrancis71 <julian.w.francis@gmail.com>
2015-06-02 16:12:29 +0100

    initial revision

---
+++ NeuralNetworks/NeuralNetwork.m
@@ -0,0 +1,59 @@
+(* ::Package:: *)
+
+(*
+  network is made up of sequence of layers
+layer is made up of biases for each of the units
+followed by the weight vector for each unit,
+ so weight is a matrix where each row is the weight vector
+for one particular unit
+
+Layer L has U units and preceeding layer has P units
+then network layer looks like (U*1,U*P)
+
+The linear activation layer looks like U*T where T is the
+number of inputs or training examples
+*)
+
+FullyConnectedForwardPropogation[inputs_,parameters_]:=(
+   Z0 = inputs;
+
+   layer1=parameters[[1]];
+   Assert[(layer1[[2,1]]//Length)==(Z0//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
+   Assert[(layer1[[1]]//Length)==(layer1[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
+   A1=layer1[[2]].Z0 + layer1[[1]];
+   Z1 = ArcTan[A1];
+
+   layer2=parameters[[2]];
+   Assert[(layer2[[2,1]]//Length)==(Z1//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
+   Assert[(layer2[[1]]//Length)==(layer2[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
+   A2=layer2[[2]].Z1 + layer2[[1]];
+   Z2 = ArcTan[A2]
+)
+
+(* INCOMPLETE *)
+(* DeltaXX refers to the partial derivative of the loss function wrt that neurone activation *)
+FullyConnectedGrad[currentParameters_,inputs_,targets_]:=(
+   DeltaZ2=2*(Z2-output);
+
+   
+)
+
+Visualise[parameters_]:=(
+
+   Z0 = Table[0,{layer1[[2,1]]//Length}];
+   Print[Z0//Length," Inputs"];
+
+   layer1=parameters[[1]];
+   Assert[(layer1[[2,1]]//Length)==(Z0//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
+   Assert[(layer1[[1]]//Length)==(layer1[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
+
+   Z1 = Table[0,{layer1[[1]]//Length}];
+   Print[Z1//Length," H1 Units"];
+
+   layer2=parameters[[2]];
+   Assert[(layer2[[2,1]]//Length)==(Z1//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
+   Assert[(layer2[[1]]//Length)==(layer2[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
+)
+
+(* Examples *)
+
