d2f39fc05cc6090f9ee386a1175ddf7af3418d46
jfrancis71 <julian.w.francis@gmail.com>
2015-06-09 16:14:44 +0100

    Reorganised to improve modularity
    Should support arbitrary levels of layers.
    Layers supported: FullyConnected1DTo1D and Convolve2D

--- NeuralNetworks/NeuralNetwork.m
+++ NeuralNetworks/NeuralNetwork.m
@@ -9,32 +9,64 @@
    followed by the weight vector for each unit,
    so weight is a matrix where each row is the weight vector
    for one particular unit
+*)
 
+ForwardPropogation[inputs_,network_]:=(
+   Z[0] = inputs;
+
+   For[layerIndex=1,layerIndex<=Length[network],layerIndex++,
+      A[layerIndex]=LayerForwardPropogation[Z[layerIndex-1],network[[layerIndex]]];
+      Z[layerIndex]=Tanh[A[layerIndex]];
+   ];
+
+   Z[layerIndex-1]
+)
+(*
    Layer L has U units and preceeding layer has P units
    then network layer looks like (U*1,U*P)
 
    The linear activation layer looks like T*U where T is the
    number of inputs or training examples
 
-   inputs has shape T*I where I is the number of inputs
+   inputs has shape T*I where I is the number of inputs or possibly just from previous layer
    output is of shape T*O
 *)
-
-FullyConnectedForwardPropogation[inputs_,parameters_]:=(
+LayerForwardPropogation[inputs_,FullyConnected1DTo1D[layerBiases_,layerWeights_]]:=(
    Z0 = inputs;
 
-   layer1=parameters[[1]];
-   Assert[(layer1[[2,1]]//Length)==(Transpose[Z0]//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
-   Assert[(layer1[[1]]//Length)==(layer1[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
-   A1=Transpose[layer1[[2]].Transpose[Z0] + layer1[[1]]];
-   Z1 = Tanh[A1];
-
-   layer2=parameters[[2]];
-   Assert[(layer2[[2,1]]//Length)==(Transpose[Z1]//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
-   Assert[(layer2[[1]]//Length)==(layer2[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
-   A2=Transpose[layer2[[2]].Transpose[Z1] + layer2[[1]]];
-   Z2 = Tanh[A2]
+   (*Weight-Activation Consistancy check*)
+   Assert[(layerWeights[[1]]//Length)==(Transpose[Z0]//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
+   (*Weight-Weight Consistancy checl*)
+   Assert[(layerBiases//Length)==(layerWeights//Length)]; (*Bias on units should match up with number of units from weight layer *)
+   A1=Transpose[layerWeights.Transpose[Z0] + layerBiases]
 )
+
+(*
+   parameters is shaped by layers where each layer is {bias,weights} where weights is a 2D kernel
+*)
+LayerForwardPropogation[inputs_,Convolve2D[layerBiases_,layerKernel_]]:=(
+   Z0 = inputs;
+
+   A1=Map[ListCorrelate[layerKernel,#]&,inputs]+layerBiases
+)
+
+BackPropogation[currentParameters_,inputs_,targets_]:=(
+
+   ForwardPropogation[inputs, currentParameters];
+   Assert[Length[currentParameters]<=2];
+   networkLayers=Length[currentParameters];
+
+   DeltaZ[networkLayers]=2*(Z[networkLayers]-targets); (*We are implicitly assuming a regression loss function*)
+   DeltaA[networkLayers]=DeltaZ[networkLayers]*Sech[A[networkLayers]]^2;
+
+   For[layerIndex=networkLayers-1,layerIndex>0,layerIndex--,
+      DeltaZ[layerIndex]=Backprop[currentParameters[[layerIndex+1]],inputs,DeltaZ[layerIndex+1]];
+      DeltaA[layerIndex]=DeltaZ[layerIndex]*Sech[A[layerIndex]]^2;
+   ];
+)
+
+Backprop[FullyConnected1DTo1D[biases_,weights_],inputs_,postLayerDeltaA_]:=Transpose[Transpose[weights].Transpose[postLayerDeltaA]]
+Backprop[Convolve2D[biases_,weights_],inputs_,postLayerDeltaA_]:=Transpose[Transpose[weights].Transpose[postLayerDeltaA]]
 
 (*
    The linear activation layer has shape T*U 
@@ -42,49 +74,29 @@ FullyConnectedForwardPropogation[inputs_,parameters_]:=(
       so it has shape T*U
    targets has shape T*O where O is the number of output units
 *)
-FullyConnectedGrad[currentParameters_,inputs_,targets_]:=(
+Grad[currentParameters_,inputs_,targets_]:=(
+   BackPropogation[currentParameters,inputs,targets];
 
-   FullyConnectedForwardPropogation[inputs, currentParameters];
-
-   DeltaZ2=2*(Z2-targets); (*We are implicitly assuming a regression loss function*)
-   DeltaA2=DeltaZ2*Sech[A2]^2;
-
-   DeltaZ1=Transpose[Transpose[layer2[[2]]].Transpose[DeltaA2]];
-   DeltaA1=DeltaZ1*Sech[A1]^2;
-
-   {{Total[Transpose[DeltaA1],{2}],Transpose[DeltaA1].inputs},{Total[Transpose[DeltaA2],{2}],Transpose[DeltaA2].Z1}}
+   Table[
+      LayerGrad[currentParameters[[layerIndex]],layerIndex]
+      ,{layerIndex,1,Length[currentParameters]}]
 )
 
+LayerGrad[FullyConnected1DTo1D[biases_,weights_],layerIndex_]:={Total[Transpose[DeltaA[layerIndex]],{2}],Transpose[DeltaA[layerIndex]].Z[layerIndex-1]}
+LayerGrad[Convolve2D[biases_,weights_],layerIndex_]:={Total[DeltaA[layerIndex],3],Apply[Plus,MapThread[ListCorrelate,{DeltaA[layerIndex],Z[layerIndex-1]}]]}
+
 (*This is implicitly a regression loss function*)
-FullyConnectedLoss[parameters_,inputs_,targets_]:=Total[(FullyConnectedForwardPropogation[inputs,parameters]-targets)^2,2]
-ConvLoss[parameters_,inputs_,targets_]:=Total[(Convolution2DForwardPropogation[inputs,parameters]-targets)^2,3]
+Loss1D[parameters_,inputs_,targets_]:=Total[(ForwardPropogation[inputs,parameters]-targets)^2,2]
+Loss2D[parameters_,inputs_,targets_]:=Total[(ForwardPropogation[inputs,parameters]-targets)^2,3]
+
+WeightDec[networkWeight_,grad_]:=(
+   Assert[Length[networkWeight]==Length[grad]];
+   Table[Head[networkWeight[[n]]][networkWeight[[n,1]]-grad[[n,1]],networkWeight[[n,2]]-grad[[n,2]]],{n,1,Length[grad]}])
 
 GradientDescent[initialParameters_,inputs_,targets_,gradientF_,lossF_,\[Lambda]_,maxLoop_:2000]:=(
    Print["Iter: ",Dynamic[loop],"Current Loss", Dynamic[lossF[wl,inputs,targets]]];
-   For[wl=initialParameters;loop=1,loop<=maxLoop,loop++,wl-=\[Lambda]*gradientF[wl,inputs,targets]];
+   For[wl=initialParameters;loop=1,loop<=maxLoop,loop++,PreemptProtect[wl=WeightDec[wl,\[Lambda]*gradientF[wl,inputs,targets]]]];
    wl )
-
-(* Note the different format from fullyconnected
-   Inputs is shaped by T*Y*X
-   parameters is shaped by layers where each layer is {bias,weights} where weights is a 2D kernel
-*)
-Convolution2DForwardPropogation[inputs_,parameters_]:=
-(
-   Z0 = inputs;
-
-   layer1=parameters[[1]];
-   A1=Map[ListCorrelate[layer1[[2]],#]&,inputs]+layer1[[1]];
-   Z1=Tanh[A1]
-)
-
-Convolution2DGrad[currentParameters_,inputs_,targets_]:=(
-   Convolution2DForwardPropogation[inputs,currentParameters];
-
-   DeltaZ1=2*(Z1-targets); (*We are implicitly assuming a regression loss function*)
-   DeltaA1=DeltaZ1*Sech[A1]^2;
-
-   {{Total[DeltaA1,3],Apply[Plus,MapThread[ListCorrelate,{DeltaA1,Z0}]]}}
-)
 
 Visualise[parameters_]:=(
 
@@ -106,39 +118,29 @@ Visualise[parameters_]:=(
 
 (* Examples *)
 sqNetwork={
-   {{.2,.3},{{2},{3}}},
-   {{.6},{{1,7}}}
+   FullyConnected1DTo1D[{.2,.3},{{2},{3}}],
+   FullyConnected1DTo1D[{.6},{{1,7}}]
 };
 sqInputs=Transpose[{Table[x,{x,0,1,0.1}]}];sqInputs//MatrixForm;
 sqOutputs=sqInputs^2;sqOutputs//MatrixForm;
-
-sqTrained:=GradientDescent[sqNetwork,sqInputs,sqOutputs,FullyConnectedGrad,FullyConnectedLoss,.0001,500000];
+sqTrained:=GradientDescent[sqNetwork,sqInputs,sqOutputs,Grad,Loss1D,.0001,500000];
 
 
 XORNetwork={
-   {{.2,.3,.7},{{2,.3},{3,.2},{1,Random[]-.5}}},
-   {{.6},{{1,Random[]-.5,Random[]-.5}}}
+   FullyConnected1DTo1D[{.2,.3,.7},{{2,.3},{3,.2},{1,Random[]-.5}}],
+   FullyConnected1DTo1D[{.6},{{1,Random[]-.5,Random[]-.5}}]
 };
 XORInputs={{0,0},{0,1},{1,0},{1,1}};XORInputs//MatrixForm;
 XOROutputs=Transpose[{{0,1,1,0}}];XOROutputs//MatrixForm;
-
-XORTrained:=GradientDescent[XORNetwork,XORInputs,XOROutputs,FullyConnectedGrad,FullyConnectedLoss,.0001,500000];
+XORTrained:=GradientDescent[XORNetwork,XORInputs,XOROutputs,Grad,Loss1D,.0001,500000];
 
 
 MultInputs=Flatten[Table[{a,b},{a,0,1,.1},{b,0,1,.1}],1];MultInputs//MatrixForm;
-
-
 MultOutputs=Map[{#[[1]]*#[[2]]}&,MultInputs];MultOutputs//MatrixForm;
+MultTrained:=GradientDescent[XORNetwork,MultInputs,MultOutputs,Grad,Loss1D,.0001,5000000];
 
 
-MultTrained:=GradientDescent[XORNetwork,MultInputs,MultOutputs,FullyConnectedGrad,FullyConnectedLoss,.0001,5000000];
-
-
-edgeNetwork={{0,Table[Random[],{3},{3}]}};
+edgeNetwork={Convolve2D[0,Table[Random[],{3},{3}]]};
 edgeInputs={StandardiseImage["C:\\Users\\Julian\\secure\\My Pictures\\me3.png"]};
-
-
-edgeOutputs=Convolution2DForwardPropogation[edgeInputs,{{0,sobelY}}];
-
-
-edgeTrained:=GradientDescent[edgeNetwork,edgeInputs,edgeOutputs,Convolution2DGrad,ConvLoss,.000001,500000]
+edgeOutputs=ForwardPropogation[edgeInputs,{Convolve2D[0,sobelY]}];
+edgeTrained:=GradientDescent[edgeNetwork,edgeInputs,edgeOutputs,Grad,Loss2D,.000001,500000]
