ee635875ea0a4e24d1950ce15714ceba8e5761d1
jfrancis71 <julian.w.francis@gmail.com>
2015-06-10 17:25:34 +0100

    Grouped layer specific code together, added ConvolveToFilterBank

--- NeuralNetworks/NeuralNetwork.m
+++ NeuralNetworks/NeuralNetwork.m
@@ -21,6 +21,79 @@ ForwardPropogation[inputs_,network_]:=(
 
    Z[layerIndex-1]
 )
+
+BackPropogation[currentParameters_,inputs_,targets_]:=(
+
+   ForwardPropogation[inputs, currentParameters];
+   Assert[Length[currentParameters]<=2];
+   networkLayers=Length[currentParameters];
+
+   DeltaZ[networkLayers]=2*(Z[networkLayers]-targets); (*We are implicitly assuming a regression loss function*)
+   DeltaA[networkLayers]=DeltaZ[networkLayers]*Sech[A[networkLayers]]^2;
+
+   For[layerIndex=networkLayers-1,layerIndex>0,layerIndex--,
+      DeltaZ[layerIndex]=Backprop[currentParameters[[layerIndex+1]],inputs,DeltaZ[layerIndex+1]];
+      DeltaA[layerIndex]=DeltaZ[layerIndex]*Sech[A[layerIndex]]^2;
+   ];
+)
+
+(*
+   The linear activation layer has shape T*U 
+   DeltaXX refers to the partial derivative of the loss function wrt that neurone activation
+      so it has shape T*U
+   targets has shape T*O where O is the number of output units
+*)
+Grad[currentParameters_,inputs_,targets_]:=(
+   BackPropogation[currentParameters,inputs,targets];
+
+   Table[
+      LayerGrad[currentParameters[[layerIndex]],layerIndex]
+      ,{layerIndex,1,Length[currentParameters]}]
+)
+
+(*This is implicitly a regression loss function*)
+Loss1D[parameters_,inputs_,targets_]:=Total[(ForwardPropogation[inputs,parameters]-targets)^2,2]
+Loss2D[parameters_,inputs_,targets_]:=Total[(ForwardPropogation[inputs,parameters]-targets)^2,3]
+Loss3D[parameters_,inputs_,targets_]:=Total[(ForwardPropogation[inputs,parameters]-targets)^2,4]
+
+(*
+WeightDec[networkWeight_,grad_]:=(
+   Assert[Length[networkWeight]==Length[grad]];
+   Table[If[Head[networkWeight[[n]]]==List,
+      Head[networkWeight[[n]]][networkWeight[[n,1]]-grad[[n,1]],networkWeight[[n,2]]-grad[[n,2]]],
+      Head[networkWeight[[n]]][MapThread[WeightDec,{networkWeight[[n]],grad[[n]]}]]],
+{n,1,Length[grad]}])*)
+WeightDec[networkLayers_List,grad_List]:=MapThread[WeightDec,{networkLayers,grad}]
+WeightDec[networkLayer_FullyConnected1DTo1D,grad_]:=FullyConnected1DTo1D[networkLayer[[1]]-grad[[1]],networkLayer[[2]]-grad[[2]]]
+WeightDec[networkLayer_Convolve2D,grad_]:=Convolve2D[networkLayer[[1]]-grad[[1]],networkLayer[[2]]-grad[[2]]]
+WeightDec[networkLayer_Convolve2DToFilterBank,grad_]:=Convolve2DToFilterBank[WeightDec[networkLayer[[1]],grad]]
+
+GradientDescent[initialParameters_,inputs_,targets_,gradientF_,lossF_,\[Lambda]_,maxLoop_:2000]:=(
+   Print["Iter: ",Dynamic[loop],"Current Loss", Dynamic[lossF[wl,inputs,targets]]];
+   For[wl=initialParameters;loop=1,loop<=maxLoop,loop++,PreemptProtect[wl=WeightDec[wl,(gw=\[Lambda]*gradientF[wl,inputs,targets])]]];
+   wl )
+
+Visualise[parameters_]:=(
+
+   Z0 = Table[0,{layer1[[2,1]]//Length}];
+   Print[Z0//Length," Inputs"];
+
+   layer1=parameters[[1]];
+   Assert[(layer1[[2,1]]//Length)==(Z0//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
+   Assert[(layer1[[1]]//Length)==(layer1[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
+
+   Z1 = Table[0,{layer1[[1]]//Length}];
+   Print[Z1//Length," H1 Units"];
+
+   layer2=parameters[[2]];
+   Assert[(layer2[[2,1]]//Length)==(Z1//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
+   Assert[(layer2[[1]]//Length)==(layer2[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
+)
+
+
+(*Layer Types*)
+
+(*FullyConnected1D Layer*)
 (*
    Layer L has U units and preceeding layer has P units
    then network layer looks like (U*1,U*P)
@@ -40,80 +113,34 @@ LayerForwardPropogation[inputs_,FullyConnected1DTo1D[layerBiases_,layerWeights_]
    Assert[(layerBiases//Length)==(layerWeights//Length)]; (*Bias on units should match up with number of units from weight layer *)
    A1=Transpose[layerWeights.Transpose[Z0] + layerBiases]
 )
+Backprop[FullyConnected1DTo1D[biases_,weights_],inputs_,postLayerDeltaA_]:=Transpose[Transpose[weights].Transpose[postLayerDeltaA]]
+LayerGrad[FullyConnected1DTo1D[biases_,weights_],layerIndex_]:={Total[Transpose[DeltaA[layerIndex]],{2}],Transpose[DeltaA[layerIndex]].Z[layerIndex-1]}
 
+(*Convolve2DLayer*)
 (*
-   parameters is shaped by layers where each layer is {bias,weights} where weights is a 2D kernel
+   layer is {bias,weights} where weights is a 2D kernel
 *)
-LayerForwardPropogation[inputs_,Convolve2D[layerBiases_,layerKernel_]]:=(
+LayerForwardPropogation[inputs_,Convolve2D[layerBias_,layerKernel_]]:=(
    Z0 = inputs;
 
-   A1=Map[ListCorrelate[layerKernel,#]&,inputs]+layerBiases
+   A1=Map[ListCorrelate[layerKernel,#]&,inputs]+layerBias
 )
-
-BackPropogation[currentParameters_,inputs_,targets_]:=(
-
-   ForwardPropogation[inputs, currentParameters];
-   Assert[Length[currentParameters]<=2];
-   networkLayers=Length[currentParameters];
-
-   DeltaZ[networkLayers]=2*(Z[networkLayers]-targets); (*We are implicitly assuming a regression loss function*)
-   DeltaA[networkLayers]=DeltaZ[networkLayers]*Sech[A[networkLayers]]^2;
-
-   For[layerIndex=networkLayers-1,layerIndex>0,layerIndex--,
-      DeltaZ[layerIndex]=Backprop[currentParameters[[layerIndex+1]],inputs,DeltaZ[layerIndex+1]];
-      DeltaA[layerIndex]=DeltaZ[layerIndex]*Sech[A[layerIndex]]^2;
-   ];
-)
-
-Backprop[FullyConnected1DTo1D[biases_,weights_],inputs_,postLayerDeltaA_]:=Transpose[Transpose[weights].Transpose[postLayerDeltaA]]
 Backprop[Convolve2D[biases_,weights_],inputs_,postLayerDeltaA_]:=Transpose[Transpose[weights].Transpose[postLayerDeltaA]]
-
-(*
-   The linear activation layer has shape T*U 
-   DeltaXX refers to the partial derivative of the loss function wrt that neurone activation
-      so it has shape T*U
-   targets has shape T*O where O is the number of output units
-*)
-Grad[currentParameters_,inputs_,targets_]:=(
-   BackPropogation[currentParameters,inputs,targets];
-
-   Table[
-      LayerGrad[currentParameters[[layerIndex]],layerIndex]
-      ,{layerIndex,1,Length[currentParameters]}]
-)
-
-LayerGrad[FullyConnected1DTo1D[biases_,weights_],layerIndex_]:={Total[Transpose[DeltaA[layerIndex]],{2}],Transpose[DeltaA[layerIndex]].Z[layerIndex-1]}
 LayerGrad[Convolve2D[biases_,weights_],layerIndex_]:={Total[DeltaA[layerIndex],3],Apply[Plus,MapThread[ListCorrelate,{DeltaA[layerIndex],Z[layerIndex-1]}]]}
 
-(*This is implicitly a regression loss function*)
-Loss1D[parameters_,inputs_,targets_]:=Total[(ForwardPropogation[inputs,parameters]-targets)^2,2]
-Loss2D[parameters_,inputs_,targets_]:=Total[(ForwardPropogation[inputs,parameters]-targets)^2,3]
+(*Convolve2DToFilterBankLayer*)
+(*
+   layer is {{bias,weights},{bias,weights},...,} where weights is a 2D kernel
+   Resulting layer is T*F*Y*X
+*)
+LayerForwardPropogation[inputs_,Convolve2DToFilterBank[filters_]]:=(
+   Z0 = inputs;
 
-WeightDec[networkWeight_,grad_]:=(
-   Assert[Length[networkWeight]==Length[grad]];
-   Table[Head[networkWeight[[n]]][networkWeight[[n,1]]-grad[[n,1]],networkWeight[[n,2]]-grad[[n,2]]],{n,1,Length[grad]}])
-
-GradientDescent[initialParameters_,inputs_,targets_,gradientF_,lossF_,\[Lambda]_,maxLoop_:2000]:=(
-   Print["Iter: ",Dynamic[loop],"Current Loss", Dynamic[lossF[wl,inputs,targets]]];
-   For[wl=initialParameters;loop=1,loop<=maxLoop,loop++,PreemptProtect[wl=WeightDec[wl,\[Lambda]*gradientF[wl,inputs,targets]]]];
-   wl )
-
-Visualise[parameters_]:=(
-
-   Z0 = Table[0,{layer1[[2,1]]//Length}];
-   Print[Z0//Length," Inputs"];
-
-   layer1=parameters[[1]];
-   Assert[(layer1[[2,1]]//Length)==(Z0//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
-   Assert[(layer1[[1]]//Length)==(layer1[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
-
-   Z1 = Table[0,{layer1[[1]]//Length}];
-   Print[Z1//Length," H1 Units"];
-
-   layer2=parameters[[2]];
-   Assert[(layer2[[2,1]]//Length)==(Z1//Length)]; (* Incoming weight matrix should match up with number of units from previous layer *)
-   Assert[(layer2[[1]]//Length)==(layer2[[2]]//Length)]; (*Bias on units should match up with number of units from weight layer *)
+   A1=Transpose[Map[LayerForwardPropogation[inputs,#]&,filters],{2,1,3,4}]
 )
+Backprop[Convolve2DToFilterBank[filters_],inputs_,postLayerDeltaA_]:=Sum[Transpose[Transpose[filter[[2]]].Transpose[postLayerDeltaA]],{filter,filters}]
+LayerGrad[Convolve2DToFilterBank[filters_],layerIndex_]:=Table[{Total[DeltaA[layerIndex][[All,filterIndex]],3],Apply[Plus,MapThread[ListCorrelate,{DeltaA[layerIndex][[All,filterIndex]],Z[layerIndex-1]}]]},{filterIndex,1,Length[filters]}]
+
 
 
 (* Examples *)
@@ -144,3 +171,8 @@ edgeNetwork={Convolve2D[0,Table[Random[],{3},{3}]]};
 edgeInputs={StandardiseImage["C:\\Users\\Julian\\secure\\My Pictures\\me3.png"]};
 edgeOutputs=ForwardPropogation[edgeInputs,{Convolve2D[0,sobelY]}];
 edgeTrained:=GradientDescent[edgeNetwork,edgeInputs,edgeOutputs,Grad,Loss2D,.000001,500000]
+
+
+edgeFilterBankNetwork={Convolve2DToFilterBank[{Convolve2D[0,Table[Random[],{3},{3}]],Convolve2D[0,Table[Random[],{3},{3}]]}]};
+edgeFilterBankOutputs=ForwardPropogation[edgeInputs,{Convolve2DToFilterBank[{Convolve2D[0,sobelY],Convolve2D[0,sobelX]}]}];
+edgeFilterBankTrained:=GradientDescent[edgeFilterBankNetwork,edgeInputs,edgeFilterBankOutputs,Grad,Loss3D,.000001,500000]
