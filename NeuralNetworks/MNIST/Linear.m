b5394b14838c4c95050a4cd77bb4ea245b4c9f7e
jfrancis71 <julian.w.francis@gmail.com>
2015-06-26 11:56:49 +0100

    initial revision

---
+++ NeuralNetworks/MNIST/Linear.m
@@ -0,0 +1,16 @@
+(* ::Package:: *)
+
+<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/NeuralNetwork.m"
+
+
+<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/MNIST/MNISTData.m"
+
+
+MNISTLinearNetwork={Adaptor2DTo1D[20],FullyConnected1DTo1D[ConstantArray[0.,10],Partition[RandomList[[1;;4000]]/4000.,400]]};
+
+
+MNISTLinearTrainingInputs=TrainingImages[[1;;50000,5;;24,5;;24]]*1.;
+MNISTLinearTrainingOutputs=TrainingLabels[[1;;50000]];
+
+
+MNISTLinearTrained:=AdaptiveGradientDescent[MNISTLinearNetwork,MNISTLinearTrainingInputs,MNISTLinearTrainingOutputs,Grad,Loss1D,500000];
