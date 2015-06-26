5d5831d49f270cf5d0a01f432dc47a075ec3e3cd
jfrancis71 <julian.w.francis@gmail.com>
2015-06-26 16:11:22 +0100

    initial revision

---
+++ NeuralNetworks/MNIST/LeNet1995.m
@@ -0,0 +1,31 @@
+(* ::Package:: *)
+
+(*
+   See Yann Le Cunn et al 1995:
+
+http://yann.lecun.com/exdb/publis/pdf/lecun-95b.pdf
+
+Network Architecture: Large Fully Connected Multi-Layer Neural Network
+                      400-300-10
+
+They claim 1.6% test result
+*)
+
+
+<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/NeuralNetwork.m"
+
+
+<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/MNIST/MNISTData.m"
+
+
+MNISTLeNet95Network={
+   Adaptor2DTo1D[20],
+   FullyConnected1DTo1D[ConstantArray[0.,300],Partition[RandomList[[1;;300*400]]/400.,400]],
+   FullyConnected1DTo1D[ConstantArray[0.,10],Partition[RandomList[[1;;3000]]/300.,300]]};
+
+
+MNISTLeNet95TrainingInputs=TrainingImages[[1;;50000,5;;24,5;;24]]*1.;
+MNISTLeNet95TrainingOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels[[1;;50000]]];
+
+
+MNISTLeNet95Trained:=AdaptiveGradientDescent[MNISTLeNet95Network,MNISTLeNet95TrainingInputs,MNISTLeNet95TrainingOutputs,Grad,Loss1D,500000];
