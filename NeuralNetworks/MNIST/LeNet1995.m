3a827c356b1e152f7bbd7ca9f6e3b1922a509a6a
jfrancis71 <julian.w.francis@gmail.com>
2015-06-29 16:17:47 +0100

    AdaptiveGradientDescent now takes options

--- NeuralNetworks/MNIST/LeNet1995.m
+++ NeuralNetworks/MNIST/LeNet1995.m
@@ -27,5 +27,13 @@ MNISTLeNet95Network={
 MNISTLeNet95TrainingInputs=TrainingImages[[1;;50000,5;;24,5;;24]]*1.;
 MNISTLeNet95TrainingOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels[[1;;50000]]];
 
+MNISTLeNet95ValidationInputs=TrainingImages[[50001;;60000,5;;24,5;;24]]*1.;
+MNISTLeNet95ValidationOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels[[50001;;60000]]];
 
-MNISTLeNet95Trained:=AdaptiveGradientDescent[MNISTLeNet95Network,MNISTLeNet95TrainingInputs,MNISTLeNet95TrainingOutputs,Grad,Loss1D,500000];
+
+ MNISTLeNet95Trained:=AdaptiveGradientDescent[
+   MNISTLeNet95Network,MNISTLeNet95TrainingInputs,MNISTLeNet95TrainingOutputs,
+   Grad,Loss1D,
+     {MaxLoop->500000,
+      ValidationInputs->MNISTLeNet95ValidationInputs,
+      ValidationTargets->MNISTLeNet95ValidationOutputs}];
