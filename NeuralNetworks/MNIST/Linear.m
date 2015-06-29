42ae71141f0288fdbdf93d821d26ac78c27172ae
jfrancis71 <julian.w.francis@gmail.com>
2015-06-29 16:12:35 +0100

    AdaptiveGradientDescent now takes options

--- NeuralNetworks/MNIST/Linear.m
+++ NeuralNetworks/MNIST/Linear.m
@@ -9,6 +9,13 @@ Network Architecture: Linear Network
                       400-10
 
 They claim 8.4% test result
+
+Our Results:
+Training Loss: .465067
+Training Classification: .83924
+
+Validation Loss: .447017
+Validation Classification: .8581
 *)
 
 
@@ -24,5 +31,9 @@ MNISTLinearNetwork={Adaptor2DTo1D[20],FullyConnected1DTo1D[ConstantArray[0.,10],
 MNISTLinearTrainingInputs=TrainingImages[[1;;50000,5;;24,5;;24]]*1.;
 MNISTLinearTrainingOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels[[1;;50000]]];
 
+MNISTLinearValidationInputs=TrainingImages[[50001;;60000,5;;24,5;;24]]*1.;
+MNISTLinearValidationOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels[[50001;;60000]]];
 
-MNISTLinearTrained:=AdaptiveGradientDescent[MNISTLinearNetwork,MNISTLinearTrainingInputs,MNISTLinearTrainingOutputs,Grad,Loss1D,500000];
+
+MNISTLinearTrained:=AdaptiveGradientDescent[MNISTLinearNetwork,MNISTLinearTrainingInputs,MNISTLinearTrainingOutputs,Grad,Loss1D,
+   {MaxLoop->500000,ValidationInputs->MNISTLinearValidationInputs,ValidationTargets->MNISTLinearValidationOutputs}];
