c397b89b910b57eb3d515957d2be4306ec6f4aef
jfrancis71 <julian.w.francis@gmail.com>
2015-07-22 11:51:22 +0100

    OK

--- NeuralNetworks/MNIST/LeNet1.m
+++ NeuralNetworks/MNIST/LeNet1.m
@@ -44,8 +44,7 @@ MNISTLeNet1={
    ],
    MaxPoolingFilterBankToFilterBank,
    Adaptor3DTo1D[12,4,4],
-   FullyConnected1DTo1D[Table[Random[],{10}],(Partition[Table[Random[],{10*192}],192]-.5)/1920],
-   Softmax
+   FullyConnected1DTo1D[Table[Random[],{10}],(Partition[Table[Random[],{10*192}],192]-.5)/1920]
 };
 
 
