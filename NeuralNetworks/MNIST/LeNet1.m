5978fa9555dfa77a8a7e9f999be5802bae7d8f24
jfrancis71 <julian.w.francis@gmail.com>
2015-07-22 11:58:27 +0100

    Put Softmax back, use wl to enable restart, improved doc,

--- NeuralNetworks/MNIST/LeNet1.m
+++ NeuralNetworks/MNIST/LeNet1.m
@@ -5,10 +5,9 @@
 
 http://yann.lecun.com/exdb/publis/pdf/lecun-95b.pdf
 
-Network Architecture: Large Fully Connected Multi-Layer Neural Network
-                      400-300-10
+Network Architecture: Convolutional Network, see Figure 1 in above paper
 
-They claim 1.6% test result
+They claim 1.7% test result
 
 Our Results:
 Iteration: 
@@ -44,7 +43,8 @@ MNISTLeNet1={
    ],
    MaxPoolingFilterBankToFilterBank,
    Adaptor3DTo1D[12,4,4],
-   FullyConnected1DTo1D[Table[Random[],{10}],(Partition[Table[Random[],{10*192}],192]-.5)/1920]
+   FullyConnected1DTo1D[Table[Random[],{10}],(Partition[Table[Random[],{10*192}],192]-.5)/1920],
+   Softmax
 };
 
 
@@ -55,8 +55,11 @@ MNISTLeNet1ValidationInputs=TrainingImages[[501;;600,1;;28,1;;28]]*1.;
 MNISTLeNet1ValidationOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels[[501;;600]]];
 
 
+wl=MNISTLeNet1;
+
+
  MNISTLeNet1Train:=AdaptiveGradientDescent[
-   MNISTLeNet1,MNISTLeNet1TrainingInputs,MNISTLeNet1TrainingOutputs,
+   wl,MNISTLeNet1TrainingInputs,MNISTLeNet1TrainingOutputs,
    Grad,ClassificationLoss,
      {MaxLoop->500000,
       ValidationInputs->MNISTLeNet1ValidationInputs,
