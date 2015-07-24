3b5d693fe3ef32cc35de6b3fb43674dd011d8b00
jfrancis71 <julian.w.francis@gmail.com>
2015-07-24 08:11:48 +0100

    updated for classification loss
    Run on whole training set.
    takes about 15 hours

--- NeuralNetworks/MNIST/Linear.m
+++ NeuralNetworks/MNIST/Linear.m
@@ -8,15 +8,15 @@ http://yann.lecun.com/exdb/publis/pdf/lecun-95b.pdf
 Network Architecture: Linear Network
                       400-10
 
-They claim 8.4% test result
+They claim 8.4% test result (i.e. 91.6% correct)
 
 Our Results:
-Iteration: 5178
-Training Loss: .462
-Training Classification: 84.1%
+Iteration: 332
+Training Loss: .339
+Training Classification: 90.5%
 
-Validation Loss: .446
-Validation Classification: 85.7%
+Validation Loss: .320
+Validation Classification: 91.1%
 *)
 
 
@@ -27,7 +27,10 @@ Validation Classification: 85.7%
 
 
 SeedRandom[1234];
-MNISTLinearNetwork={Adaptor2DTo1D[20],FullyConnected1DTo1D[ConstantArray[0.,10],Partition[Table[Random[],{4000}]/4000.,400]]};
+MNISTLinearNetwork={
+   Adaptor2DTo1D[20],
+   FullyConnected1DTo1D[ConstantArray[0.,10],Partition[Table[Random[],{4000}]/4000.,400]],
+   Softmax};
 
 
 MNISTLinearTrainingInputs=TrainingImages[[1;;50000,5;;24,5;;24]]*1.;
@@ -37,5 +40,5 @@ MNISTLinearValidationInputs=TrainingImages[[50001;;60000,5;;24,5;;24]]*1.;
 MNISTLinearValidationOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels[[50001;;60000]]];
 
 
-MNISTLinearTrain:=AdaptiveGradientDescent[MNISTLinearNetwork,MNISTLinearTrainingInputs,MNISTLinearTrainingOutputs,Grad,RegressionLoss1D,
+MNISTLinearTrain:=AdaptiveGradientDescent[MNISTLinearNetwork,MNISTLinearTrainingInputs,MNISTLinearTrainingOutputs,Grad,ClassificationLoss,
    {MaxLoop->500000,ValidationInputs->MNISTLinearValidationInputs,ValidationTargets->MNISTLinearValidationOutputs}];
