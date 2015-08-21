2ce03c2c1167ea6c40f69f269454f06f6f0a66b5
jfrancis71 <julian.w.francis@gmail.com>
2015-08-21 08:57:32 +0100

    Adding Net2

--- NeuralNetworks/ImgEncode/ImgEncode.m
+++ NeuralNetworks/ImgEncode/ImgEncode.m
@@ -16,8 +16,14 @@ Net1={
    FullyConnected1DTo1D[Table[Random[],{32*32}],Table[Random[]-.5,{32*32},{100}]]
 };
 
+Net2={
+   FullyConnected1DTo1D[Table[-10,{100}]-.5,Table[(Random[]-.5),{100},{32*32}]/(1000*32*32)],
+   Logistic,
+   FullyConnected1DTo1D[Table[.2759,{32*32}],Table[Random[]-.5,{32*32},{100}]/1000000]
+};
 
-wl=Net1;
+
+wl=Net2;
 TrainingHistory={};
 ValidationHistory={};
 
@@ -32,6 +38,18 @@ Net1KTanhTrain:=(
       wl,Fl[[1;;1000]],Fl[[1;;1000]],
       BatchGrad,RegressionLoss1D,
         {MaxLoop->500000,
-         UpdateFunction->SkipWebMonitor[name],
+         UpdateFunction->SkipWebMonitor[name,100],
+         InitialLearningRate->\[Lambda]}];
+)
+
+
+Net2Sz1KLogTrain:=(
+   name="ImgEncode\\Net2Sz1KLog";
+   {TrainingHistory,ValidationHistory,wl,\[Lambda]}=Import[StringJoin["C:\\Users\\Julian\\Documents\\GitHub\\Machine-Vision\\NeuralNetworks\\",name,".wdx"]];
+   AdaptiveGradientDescent[
+      wl,Fl[[1;;1000]],Fl[[1;;1000]],
+      BatchGrad,RegressionLoss1D,
+        {MaxLoop->50000,
+         UpdateFunction->SkipWebMonitor[name,100],
          InitialLearningRate->\[Lambda]}];
 )
