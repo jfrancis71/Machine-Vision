e7d8ca23a54b138b05d578b019d52117b3f633c3
jfrancis71 <julian.w.francis@gmail.com>
2015-08-30 16:07:52 +0100

    Fixed bug in using different loss functions

--- NeuralNetworks/CIFAR10/Autoencoder.m
+++ NeuralNetworks/CIFAR10/Autoencoder.m
@@ -1,15 +1,17 @@
 (* ::Package:: *)
 
 (* Learns a basic denoising autoencoder trained on CIFAR-10
-   It doesn't produce the interesting filters reported in the literature.
-   However it does seem to do a pretty good job of denoising a corrupted input.
-   Loss around 9.9 which presumably corresponds to mean pixel error of around 10%
+   Does replicate learning features observed in the literature even if it is not exactly Gabor like.
+   It does seem to do a pretty good job of denoising a corrupted input, albeit output is somewhat blurred.
+   Subjectively the noise is quite severe and I would have a hard time reconstructing the original image.
+   Loss around 8.4 after 1,600 epochs
+   which presumably corresponds to mean pixel error of around 10%
 
    Ref: http://deeplearning.net/tutorial/dA.html
 *)
 
 
-<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/NeuralNetwork.m"
+<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/Autoencoder.m"
 
 
 <<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/CIFAR10/CIFAR10Data.m"
@@ -32,8 +34,8 @@ Fl=Map[Flatten,Images];
 
 
 TiedGrad[wl_,inputs_,targets_,lossF_]:=(
-   t1=ReplacePart[NNGrad[ReplacePart[wl,{3,2}->Transpose[wl[[1,2]]]],inputs,targets,CrossEntropyLoss],{3,2}->wl[[3,2]]*0.0];
-   t2=Transpose[NNGrad[ReplacePart[wl,{3,2}->Transpose[wl[[1,2]]]],inputs,targets,CrossEntropyLoss][[3,2]]];
+   t1=ReplacePart[NNGrad[ReplacePart[wl,{3,2}->Transpose[wl[[1,2]]]],inputs,targets,RegressionLoss1D],{3,2}->wl[[3,2]]*0.0];
+   t2=Transpose[NNGrad[ReplacePart[wl,{3,2}->Transpose[wl[[1,2]]]],inputs,targets,RegressionLoss1D][[3,2]]];
    t3=t1;
    t4=t3;
    t4[[1,2]]+=t2;
@@ -62,5 +64,5 @@ Train:=MiniBatchGradientDescent[
         {MaxEpoch->500000,
          ValidationInputs->Fl[[4001;;4500]],
          ValidationTargets->Fl[[4001;;4500]],         
-         InitialLearningRate->.001}];
+         InitialLearningRate->.01}];
 
