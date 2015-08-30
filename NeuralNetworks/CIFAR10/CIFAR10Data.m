83f8880ab89d194f3dfbe0ecf4a2eccd02a0f6f5
jfrancis71 <julian.w.francis@gmail.com>
2015-08-30 08:59:47 +0100

    reads in all CIFAR data
    code restructuring to reduce inefficiencies

--- NeuralNetworks/CIFAR10/CIFAR10Data.m
+++ NeuralNetworks/CIFAR10/CIFAR10Data.m
@@ -11,44 +11,34 @@
 <<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/NeuralNetwork.m"
 
 
-dat=Join[
-   BinaryReadList["C:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_1.bin"](*,
-   BinaryReadList["C:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_2.bin"],
-   BinaryReadList["C:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_3.bin"],
-   BinaryReadList["C:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_4.bin"],
-   BinaryReadList["C:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_5.bin"]*)];
-
-
-records=Partition[dat,3073];
-
-
-reorder=records;(*Actually decided to retain original order to do with reproducibility *)
-
-
-labels=Map[First,reorder];
-
-
-picVector=Map[Rest,reorder];
-
-
-TrainingImages=Table[((Partition[Sqrt[Total[Partition[picVector[[l]],1024]^2]],32]//Reverse)//N)/443,
-{l,1,10000}];
-
-
-TrainingLabels=labels[[1;;4500]];
-
-
-ValidationImages=Table[((Partition[Sqrt[Total[Partition[picVector[[l]],1024]^2]],32]//Reverse)//N)/443,
-{l,4501,5000}];
-
-
-ValidationLabels=labels[[4501;;5000]];
-
-
-ColTrainingImages=Table[Map[Partition[#,32]&,Partition[picVector[[l]],1024]]/256.,{l,1,10000}];
-
-
-ColValidationImages=Table[Map[Partition[#,32]&,Partition[picVector[[l]],1024]]/256.,{l,4501,5000}];
-
-
 WordLabels={"airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"};
+
+
+CIFARReadFile[filename_]:={
+Map[First,Partition[BinaryReadList[filename],3073]],
+Map[Function[{flat},Partition[flat,32]],Map[Partition[#,1024]&,Map[Rest,Partition[BinaryReadList[filename],3073]]],{2}]/256.
+}
+
+
+CIFARFiles={"c:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_1.bin",
+   "c:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_2.bin",
+   "c:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_3.bin",
+   "c:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_4.bin"
+};
+
+
+TrainingLabels=Flatten[
+   Map[CIFARReadFile,CIFARFiles]
+   [[All,1]]];
+
+
+ColTrainingImages=Flatten[
+   Map[CIFARReadFile,
+   CIFARFiles]
+   [[All,2]],1];
+
+
+ValidationLabels=CIFARReadFile["c:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_5.bin"][[1]];
+
+
+ColValidationImages=CIFARReadFile["c:\\Users\\Julian\\ImageDataSetsPublic\\CIFAR-10\\data_batch_5.bin"][[2]];
