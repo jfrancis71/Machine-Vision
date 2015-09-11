82ce54cb8433a1f905e485aef2aca305f76cf547
jfrancis71 <julian.w.francis@gmail.com>
2015-09-11 09:53:18 +0100

    Added salient function
    Also bugfix to DeltaLoss and back prop misses final softmax layer to
    avoid achieving high classification rate by minimising other classes

--- NeuralNetworks/MNIST/Dream.m
+++ NeuralNetworks/MNIST/Dream.m
@@ -7,7 +7,7 @@
 NNRead["MNIST\\LeNet1"];
 
 
-DeltaLoss[DreamLoss,outputs_,targets_]:={target*1.};
+DeltaLoss[DreamLoss,outputs_,targets_]:=targets*1.;
 
 
 DreamLoss[parameters_,inputs_,targets_]:=Total[Extract[ForwardPropogate[inputs,parameters],Position[targets,1]]]/Length[inputs];
@@ -16,14 +16,23 @@ DreamLoss[parameters_,inputs_,targets_]:=Total[Extract[ForwardPropogate[inputs,p
 Dream[f_]:=(
    target=ReplacePart[ConstantArray[0,10],f->1];
    dream=Table[Random[],{20},{20}];
-   For[sl=0,sl<=2000,sl++,
-      BackPropogation[wl[[1;;-1]],{dream},{target},DreamLoss];
+   For[sl=0,sl<=200000,sl++,
+      BackPropogation[wl[[1;;-2]],{dream},{target},DreamLoss];
       dw=Backprop[wl[[1]],DeltaL[1]];
       fw=UnitStep[dream-1.];
       ef=UnitStep[-dream];
-      dream += (.01*dw[[1]]/Max[dw[[1]]]-fw+ef)*1.0*10^-1;
+      (*dream += (.01*dw[[1]]/Max[dw[[1]]]-fw+ef)*1.0*10^-1;*)
+      dream += .1*dw[[1]];
+      dream = dream/Norm[dream];
       (*Clip[dream,{0.,1.}];*)
       error=DreamLoss[wl[[1;;-1]],{dream},{target}];
 ];
    dream
+)
+
+
+Salient[f_,image_]:=(
+   BackPropogation[wl[[1;;-1]],{image},{f},DreamLoss];
+   dw=Backprop[wl[[1]],DeltaL[1]][[1]];
+   dw/Max[dw]
 )
