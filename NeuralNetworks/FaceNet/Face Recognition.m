d975c6cbbc4fada3e2919cb3c60483fab0fdeda9
jfrancis71 <julian.w.francis@gmail.com>
2015-10-01 14:05:57 +0100

    Added FaceDetection
    designed for use by camera

--- NeuralNetworks/FaceNet/Face Recognition.m
+++ NeuralNetworks/FaceNet/Face Recognition.m
@@ -1,14 +1,20 @@
 (* ::Package:: *)
 
-(* ::Input:: *)
-(*PlotNNFaceRecognition[image_]:=( *)
-(*(* We don't convolve with whole image. Issue appears regarding the padding around our filter operations *)*)
-(**)
-(*   partitionImage=Partition[image,{32,32},{4,4}];*)
-(*   flattenImage=Flatten[partitionImage,1];*)
-(*   processed=ForwardPropogate[flattenImage,wl][[All,1]];*)
-(*   tableProc=Partition[processed,Length[partitionImage[[1]]]];*)
-(*   pos=Map[Reverse,Position[tableProc,z_/;z>.9]];*)
-(*   centres=Map[(#-{1,1})*4+{16,16}&,pos];*)
-(*   Show[image//DispImage,OutlineGraphics[BoundingRectangles[centres,{16,16}]]]*)
-(*)*)
+PlotNNFaceRecognition[image_]:=( 
+(* We don't convolve with whole image. Issue appears regarding the padding around our filter operations *)
+
+   partitionImage=Partition[image,{32,32},{4,4}];
+   flattenImage=Flatten[partitionImage,1];
+   processed=ForwardPropogate[flattenImage,wl][[All,1]];
+   tableProc=Partition[processed,Length[partitionImage[[1]]]];
+   pos=Map[Reverse,Position[tableProc,z_/;z>.9]];
+   centres=Map[(#-{1,1})*4+{16,16}&,pos];
+   Show[image//DispImage,OutlineGraphics[BoundingRectangles[centres,{16,16}]]]
+)
+
+
+FaceDetection[image_]:=(
+   rey=ImageData[ImageResize[image//Image,43]][[All,6;;-7]];
+   rec=ForwardPropogate[{ImageData[ImageResize[image//Image,43]][[All,6;;-7]]},wl];
+   {rey//DispImage,BarChart[rec,PlotRange->{0,1}]}
+)
