83429582be05b0690a90cd8e9b9bc44ed3d522ac
jfrancis71 <julian.w.francis@gmail.com>
2015-10-12 15:58:12 +0100

    initial revision

---
+++ NeuralNetworks/CIFAR10/Net5MB.m
@@ -0,0 +1,60 @@
+(* ::Package:: *)
+
+(*
+
+   Ref: https://code.google.com/p/cuda-convnet/
+   Ref: https://code.google.com/p/cuda-convnet/source/browse/trunk/example-layers/layers-18pct.cfg
+   Ref: Alex Krizhevsky
+
+   Not completely faithful implementation.
+   He uses RELU and a contrast normalisation layer.
+*)
+
+
+<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/NeuralNetwork.m"
+
+
+<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/CIFAR10/CIFAR10Data.m"
+
+
+SeedRandom[1234];
+CIFARNet5={
+   PadFilterBank[2],ConvolveFilterBankToFilterBankInit[3,32,5],Tanh,
+   MaxConvolveFilterBankToFilterBank,SubsampleFilterBankToFilterBank,
+   PadFilterBank[2],ConvolveFilterBankToFilterBankInit[32,32,5],Tanh,
+   MaxConvolveFilterBankToFilterBank,SubsampleFilterBankToFilterBank,
+   PadFilterBank[2],ConvolveFilterBankToFilterBankInit[32,64,5],Tanh,
+   MaxConvolveFilterBankToFilterBank,SubsampleFilterBankToFilterBank,
+   Adaptor3DTo1D[64,4,4],
+   FullyConnected1DTo1DInit[64*4*4,10],
+   Softmax
+};
+
+
+CIFAR10NetTrainingInputs=ColTrainingImages[[1;;4000]]*1.;
+CIFAR10NetTrainingOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,TrainingLabels];
+
+CIFAR10NetValidationInputs=ColValidationImages[[All]]*1.;
+CIFAR10NetValidationOutputs=Map[ReplacePart[ConstantArray[0,10],(#+1)->1]&,ValidationLabels];
+
+
+wl=CIFARNet5;
+TrainingHistory={};
+ValidationHistory={};
+\[Lambda]=.01;
+
+
+TrainNet5MB:=MiniBatchGradientDescent[
+      wl,ColTrainingImages,CIFAR10NetTrainingOutputs,
+      NNGrad,ClassificationLoss,
+        {MaxEpoch->500000,
+         ValidationInputs->ColValidationImages[[1;;500]],
+         ValidationTargets->CIFAR10NetValidationOutputs[[1;;500]],
+         UpdateFunction->NNCheckpoint["CIFAR10\\Net5MB"],
+         InitialLearningRate->\[Lambda]}];
+
+
+CIFAR10Output[probs_]:=BarChart[probs[[Reverse[Ordering[probs,-3]]]],ChartLabels->WordLabels[[Reverse[Ordering[probs,-3]]]]];
+
+
+CIFAR10Outputs[probs_,pics_]:=MapThread[{Image[#2,Interleaving->False],CIFAR10Output[#1]}&,{probs,pics}];
