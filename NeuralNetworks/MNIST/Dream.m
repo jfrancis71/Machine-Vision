3c4759b091edae9f6e34f37bfa2599146657a37e
jfrancis71 <julian.w.francis@gmail.com>
2015-10-13 12:24:38 +0100

    More interesting to start from .5

--- NeuralNetworks/MNIST/Dream.m
+++ NeuralNetworks/MNIST/Dream.m
@@ -21,7 +21,7 @@ Options[Dream] = { MaxIterations -> 2000 };
 
 
 Dream[inputDims_,neuron_,opts___]:=(
-   dream=Array[Random[]&,inputDims];
+   dream=Array[.5&,inputDims];
    neuronLayer=neuron[[1]];
    target=ReplacePart[ForwardPropogate[{dream},wl[[1;;neuronLayer]]][[1]]*.0,Rest[neuron]->1.0];
    For[sl=0,sl<=(MaxIterations/.{opts}/.Options[Dream]),sl++,
