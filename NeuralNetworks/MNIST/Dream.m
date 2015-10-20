1c335ff9a39f8cfe23fd04827366860581e176c8
jfrancis71 <julian.w.francis@gmail.com>
2015-10-20 11:47:00 +0100

    use new Mathematica options style, drop MNIST dependancy

--- NeuralNetworks/MNIST/Dream.m
+++ NeuralNetworks/MNIST/Dream.m
@@ -1,10 +1,6 @@
 (* ::Package:: *)
 
 <<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/NeuralNetwork.m"
-<<"C:/users/julian/documents/github/Machine-Vision/NeuralNetworks/MNIST/MNISTData.m"
-
-
-NNRead["MNIST\\LeNet1"];
 
 
 DeltaLoss[DreamLoss,outputs_,targets_]:=targets*1.;
@@ -17,20 +13,20 @@ Dream::usage = "Dream[inputDims,neuron] returns input which maximises neuron res
 Note if neuron is saturated, then gradients may have difficulty propogating and optimisation will be exceptionally slow";
 
 
-Options[Dream] = { MaxIterations -> 2000 };
+Options[Dream] = { MaxIterations -> 2000, InitialLearningRate->.01 };
 
 
-Dream[inputDims_,neuron_,opts___]:=(
+Dream[inputDims_,neuron_,opts:OptionsPattern[]]:=(
    dream=Array[.5&,inputDims];
    neuronLayer=neuron[[1]];
    target=ReplacePart[ForwardPropogate[{dream},wl[[1;;neuronLayer]]][[1]]*.0,Rest[neuron]->1.0];
-   For[sl=0,sl<=(MaxIterations/.{opts}/.Options[Dream]),sl++,
+   For[sl=0,sl<=OptionValue[MaxIterations],sl++,
       BackPropogation[wl[[1;;neuronLayer]],{dream},{target},DreamLoss];
       dw=BackPropogateLayer[wl[[1]],DeltaL[1]];
       fw=UnitStep[dream-1.];
       ef=UnitStep[-dream];
       (*dream += (.01*dw[[1]]/Max[dw[[1]]]-fw+ef)*1.0*10^-1;*)
-      dream += .01*dw[[1]];
+      dream += OptionValue[InitialLearningRate]*dw[[1]];
       (*dream = dream/Norm[dream];*)
       (*Clip[dream,{0.,1.}];*)
       error=DreamLoss[wl[[1;;-1]],{dream},{target}];
