7943076555dad83fd0f5be31f01ef956f758b0ed
jfrancis71 <julian.w.francis@gmail.com>
2016-01-16 15:17:42 +0000

    added labelling to monitor output

--- NeuralNetworks/ChessPieces/ChessRecognition.m
+++ NeuralNetworks/ChessPieces/ChessRecognition.m
@@ -6,9 +6,12 @@
 NNRead["ChessPieces\\ChessPiecesMultiple"]
 
 
+NNCategoryOutput[probs_,labels_]:=BarChart[probs,ChartLabels->labels];
+
+
 ChessRecognition[stationNo_Integer]:=
    MobileRecognition[
       (piece=currentImg[[1;;1+64-1;;2,32;;32+64-1;;2]];
-      {ForwardPropogate[{piece},wl],currentImg//DispImage}
+      {f=ForwardPropogate[{piece},wl];NNCategoryOutput[f//First,{"Empty","Pawn","Rook","King"}],currentImg//DispImage}
       )&,stationNo
 ];
