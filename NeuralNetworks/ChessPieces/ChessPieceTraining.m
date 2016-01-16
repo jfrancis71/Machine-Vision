33093d47ec9bfc670e4e07870af2756c8ff54c31
jfrancis71 <julian.w.francis@gmail.com>
2016-01-16 15:15:30 +0000

    added new piece

--- NeuralNetworks/ChessPieces/ChessPieceTraining.m
+++ NeuralNetworks/ChessPieces/ChessPieceTraining.m
@@ -17,14 +17,23 @@
 chessDat=Import["C:\\Users\\julian\\ImageDataSetsPublic\\ChessPiece\\ChessPieceImages.wdx"];
 
 
-RawTrainingImages=Flatten[chessDat,1];
+RawTrainingImages=Flatten[chessDat[[{1,2,4,5}]],1];
+
+
+(*RawTrainingLabels=Join[
+   ConstantArray[0,Length[chessDat[[1]]]],
+   ConstantArray[1,Length[chessDat[[2]]]],
+   ConstantArray[2,Length[chessDat[[3]]]],
+   ConstantArray[3,Length[chessDat[[4]]]],
+   ConstantArray[4,Length[chessDat[[5]]]]
+];*)
 
 
 RawTrainingLabels=Join[
    ConstantArray[0,Length[chessDat[[1]]]],
    ConstantArray[1,Length[chessDat[[2]]]],
-   ConstantArray[2,Length[chessDat[[3]]]]
-];
+   ConstantArray[2,Length[chessDat[[4]]]],
+   ConstantArray[3,Length[chessDat[[5]]]]];
 
 
 SeedRandom[1234];
@@ -33,7 +42,7 @@ samp=RandomSample[Transpose[{RawTrainingImages,RawTrainingLabels}]];
 
 ChessPieceImages=samp[[All,1]];
 ChessPieceLabels=Table[
-   ReplacePart[ConstantArray[0.,3],(samp[[s,2]]+1)->1],{s,1,Length[samp]}];
+   ReplacePart[ConstantArray[0.,4],(samp[[s,2]]+1)->1],{s,1,Length[samp]}];
 
 
 SeedRandom[1234];
@@ -45,7 +54,7 @@ ChessPieceNet={
    PadFilterBank[2],ConvolveFilterBankToFilterBankInit[32,64,5],Tanh,
    MaxPoolingFilterBankToFilterBank,
    Adaptor3DTo1D[64,4,4],
-   FullyConnected1DTo1DInit[64*4*4,3],
+   FullyConnected1DTo1DInit[64*4*4,4],
    Softmax
 };
 
@@ -57,11 +66,11 @@ ValidationHistory={};
 
 
 TrainChessPiecesNet:=MiniBatchGradientDescent[
-      wl,ChessPieceImages[[1;;1000]],ChessPieceLabels[[1;;1000]],
+      wl,ChessPieceImages[[1;;1400]],ChessPieceLabels[[1;;1400]],
       NNGrad,CrossEntropyLoss,
         {MaxEpoch->500000,
-         ValidationInputs->ChessPieceImages[[1001;;-1]],
-         ValidationTargets->ChessPieceLabels[[1001;;-1]],
+         ValidationInputs->ChessPieceImages[[1401;;-1]],
+         ValidationTargets->ChessPieceLabels[[1401;;-1]],
          StepMonitor->NNCheckpoint["ChessPieces\\ChessPiecesMultiple"],
          Momentum->.9,MomentumType->"Nesterov",
          InitialLearningRate->\[Lambda]}];
