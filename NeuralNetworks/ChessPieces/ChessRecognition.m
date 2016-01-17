a00ff158e30cfb102fc08e301596cf28bcf1c2d8
jfrancis71 <julian.w.francis@gmail.com>
2016-01-17 08:48:01 +0000

    Adding saliency map

--- NeuralNetworks/ChessPieces/ChessRecognition.m
+++ NeuralNetworks/ChessPieces/ChessRecognition.m
@@ -12,6 +12,7 @@ NNCategoryOutput[probs_,labels_]:=BarChart[probs,ChartLabels->labels];
 ChessRecognition[stationNo_Integer]:=
    MobileRecognition[
       (piece=currentImg[[1;;1+64-1;;2,32;;32+64-1;;2]];
-      {f=ForwardPropogate[{piece},wl];NNCategoryOutput[f//First,{"Empty","Pawn","Rook","King"}],currentImg//DispImage}
+      {f=ForwardPropogate[{piece},wl];NNCategoryOutput[f//First,{"Empty","Pawn","Rook","King"}],currentImg//DispImage,
+{piece//DispImage,Map[(Salient[#,piece]//DispImage)&,Table[ReplacePart[ConstantArray[0,4],tr->1],{tr,1,4}]]}}
       )&,stationNo
 ];
