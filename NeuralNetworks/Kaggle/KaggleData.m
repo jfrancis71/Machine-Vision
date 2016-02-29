65403cd41870dd8573e30ef50e6b97b863eb1522
jfrancis71 <julian.w.francis@gmail.com>
2016-02-29 08:51:24 +0000

    remove training examples without data

--- NeuralNetworks/Kaggle/KaggleData.m
+++ NeuralNetworks/Kaggle/KaggleData.m
@@ -6,31 +6,36 @@
 data=Import["C:\\Users\\Julian\\ImageDataSetsPublic\\FacialKeypoints\\training\\training.csv"];
 
 
-faceImages=Table[(Partition[ReadList[StringToStream[data[[t,31]]],Number],96]/255.),{t,2,7049}];
+faceImages=Table[(Partition[ReadList[StringToStream[data[[t,31]]],Number],96]/255.),{t,2,7050}];
 
 
-drawFace[face_,keyP_]:=
-   Show[face//Image,Graphics[{Red,Point[{keyP[[1]],96-keyP[[2]]}],Point[{keyP[[3]],96-keyP[[4]]}]}]]
+faceKeypoints=data[[2;;-1,1;;30]];
 
 
-images=ReadImagesFromDirectory["C:\\Users\\julian\\Google Drive\\Personal\\Pictures\\Coolpix\\*\\"];
-
-
-patches=Flatten[Map[Partition[#,{32,32}]&,images],2];
+(*drawFace[face_,keyP_]:=
+   Show[face//Image,Graphics[{Red,Point[{keyP[[1]],96-keyP[[2]]}],Point[{keyP[[3]],96-keyP[[4]]}]}]]*)
 
 
 kaggleImages=Map[Reverse[ImageData[ImageResize[Image[#],{32,32}]]]&,faceImages];
 
 
-RawTrainingImages=Join[kaggleImages,patches];
+RawTrainingLabels=faceKeypoints[[All,1;;4]]*32/96;
 
 
-RawTrainingLabels=Join[ConstantArray[1,Length[kaggleImages]],ConstantArray[0,Length[patches]]];
+dirty=Position[Map[VectorQ[#,MachineNumberQ]&,RawTrainingLabels],False];
+
+
+procImages=Delete[kaggleImages,dirty];
+procLabels=Delete[RawTrainingLabels,dirty];
 
 
 SeedRandom[1234];
-samp=RandomSample[Transpose[{RawTrainingImages,RawTrainingLabels}]];
+samp=RandomSample[Transpose[{procImages,procLabels}]];
+
+
+drawFace[face_,keyP_]:=
+Show[face//DispImage,Graphics[{Red,Point[{keyP[[1]],32-keyP[[2]]}],Point[{keyP[[3]],32-keyP[[4]]}]}]]
 
 
 FaceImages=samp[[All,1]];
-FaceLabels=Map[{#}&,samp[[All,2]]];
+FaceLabels=Map[{#[[1]],#[[2]],#[[3]],#[[4]]}&,samp[[All,2]]];
